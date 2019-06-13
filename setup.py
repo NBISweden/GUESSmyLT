dd0953325aec2784452cc455f07b715b964e16ff
Jacques Dainat <jacques.dainat@nbis.se>
2019-06-13 11:46:48 +0200

    update to 0.2.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='guessmylt',
-    version='0.2.4',
+    version='0.2.5',
 
     description='An efficient way to guess the library type of RNA-reads',
 
